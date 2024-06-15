44c47dfb0c401f96523c2579bb1de592e7e99846
bekurin <bekurin17@gmail.com>
2024-06-15 13:29:08 +0900

    feat: init project

---
+++ src/test/kotlin/deferred/payment/platform/PlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package deferred.payment.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
